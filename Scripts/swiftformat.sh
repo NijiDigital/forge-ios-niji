c7d9db03557083ba0dba9cccb8ffaed53860f888
Mathieu VIEL <mathieu.viel@niji.fr>
2023-06-14 15:27:13 +0200

    checking in shell

--- Scripts/swiftformat.sh
+++ Scripts/swiftformat.sh
@@ -8,7 +8,13 @@ export PATH="$PATH:/opt/homebrew/bin"
 if [ "${CONFIGURATION}" == "Debug" ]; then
   echo "Running Swift Format"
   if which swiftformat > /dev/null; then
-    swiftformat . --config "$SRCROOT/fastlane/forge/.swiftformat" --exclude "$SRCROOT/fastlane/" --exclude "$SRCROOT/Pods/" --exclude "**/Generated/" --exclude "**/Templates/" --exclude "Build/" --exclude "Reports/" --exclude ".build/" --exclude "DerivedData/"
+    CONFIG_FILE="$SRCROOT/fastlane/forge/.swiftformat"
+    if [ -f "$SRCROOT/.swiftformat" ]; then
+        echo "Switching to local .swiftformat"
+        CONFIG_FILE="$SRCROOT/.swiftformat"
+    fi
+    echo "CONFIG : $CONFIG_FILE"
+    swiftformat . --config $CONFIG_FILE --exclude "$SRCROOT/fastlane/" --exclude "$SRCROOT/Pods/" --exclude "**/Generated/" --exclude "**/Templates/" --exclude "Build/" --exclude "Reports/" --exclude ".build/" --exclude "DerivedData/"
   else
     echo "warning: SwiftFormat not installed, download from https://github.com/nicklockwood/SwiftFormat"
   fi
